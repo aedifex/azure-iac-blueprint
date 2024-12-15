8dae95bb10b013f480af5632d05080c7b6110cc6
aedifex <aedifex@gmail.com>
2024-12-15 11:51:50 -0800

    Update provider block

--- infra/providers.tf
+++ infra/providers.tf
@@ -15,4 +15,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id       = "<+connector.name.clientId>"
+  client_secret   = "<+secrets.getValue('Azure Sandbox')>"
+  tenant_id       = "<+connector.name.tenantId>"
+  subscription_id = "e3bf43a8-eb94-4b99-8be8-6e873d3ac0ee" # Explicit subscription ID
 }
